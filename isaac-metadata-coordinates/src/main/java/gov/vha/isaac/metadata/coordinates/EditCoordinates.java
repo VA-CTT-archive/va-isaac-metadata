0d3baa2daf95a5ee13b4d8fb7959a2fa8ef590bb
Keith E. Campbell <campbell@informatics.com>
2015-04-06 21:39:42 -0700

    Adding new concepts and update for release.

--- isaac-metadata-coordinates/src/main/java/gov/vha/isaac/metadata/coordinates/EditCoordinates.java
+++ isaac-metadata-coordinates/src/main/java/gov/vha/isaac/metadata/coordinates/EditCoordinates.java
@@ -1,40 +1,57 @@
 package gov.vha.isaac.metadata.coordinates;
 
 import gov.vha.isaac.metadata.source.IsaacMetadataAuxiliaryBinding;
+import gov.vha.isaac.ochre.api.IdentifierService;
+import gov.vha.isaac.ochre.api.LookupService;
 import gov.vha.isaac.ochre.api.coordinate.EditCoordinate;
 import gov.vha.isaac.ochre.model.coordinate.EditCoordinateImpl;
+import org.ihtsdo.otf.tcc.api.spec.ConceptSpec;
 
-import java.io.IOException;
 
 /**
  * Created by kec on 2/16/15.
  */
 public class EditCoordinates {
+    
+    private static IdentifierService identifierService = null;
+    private static IdentifierService getIdentifierService() {
+        if (identifierService == null) {
+            identifierService = LookupService.getService(IdentifierService.class);
+        }
+        return identifierService;
+    }
+    
+    private static int getNid(ConceptSpec spec) {
+        return getIdentifierService().getNidForUuids(spec.getUuids());
+    }
 
-    public static EditCoordinate getDefaultUserSolorOverlay() throws IOException {
+    public static EditCoordinate getDefaultUserSolorOverlay() {
 
-        EditCoordinate editCoordinate = new EditCoordinateImpl(IsaacMetadataAuxiliaryBinding.USER.getLenient().getNid(),
-                IsaacMetadataAuxiliaryBinding.SOLOR_OVERLAY.getLenient().getNid(),
-                IsaacMetadataAuxiliaryBinding.DEVELOPMENT.getLenient().getNid());
+        EditCoordinate editCoordinate = new EditCoordinateImpl(
+                getNid(IsaacMetadataAuxiliaryBinding.USER),
+                getNid(IsaacMetadataAuxiliaryBinding.SOLOR_OVERLAY),
+                getNid(IsaacMetadataAuxiliaryBinding.DEVELOPMENT));
 
         return editCoordinate;
     }
 
 
-    public static EditCoordinate getDefaultUserVeteransAdministrationExtension() throws IOException {
+    public static EditCoordinate getDefaultUserVeteransAdministrationExtension() {
 
-        EditCoordinate editCoordinate = new EditCoordinateImpl(IsaacMetadataAuxiliaryBinding.USER.getLenient().getNid(),
-                IsaacMetadataAuxiliaryBinding.VHA.getLenient().getNid(),
-                IsaacMetadataAuxiliaryBinding.DEVELOPMENT.getLenient().getNid());
+        EditCoordinate editCoordinate = new EditCoordinateImpl(
+                getNid(IsaacMetadataAuxiliaryBinding.USER),
+                getNid(IsaacMetadataAuxiliaryBinding.VHA),
+                getNid(IsaacMetadataAuxiliaryBinding.DEVELOPMENT));
 
         return editCoordinate;
     }
 
-    public static EditCoordinate getDefaultUserMetadata() throws IOException {
+    public static EditCoordinate getDefaultUserMetadata()  {
 
-        EditCoordinate editCoordinate = new EditCoordinateImpl(IsaacMetadataAuxiliaryBinding.USER.getLenient().getNid(),
-                IsaacMetadataAuxiliaryBinding.ISAAC_METADATA_MODULE.getLenient().getNid(),
-                IsaacMetadataAuxiliaryBinding.DEVELOPMENT.getLenient().getNid());
+        EditCoordinate editCoordinate = new EditCoordinateImpl(
+                getNid(IsaacMetadataAuxiliaryBinding.USER),
+                getNid(IsaacMetadataAuxiliaryBinding.ISAAC_METADATA_MODULE),
+                getNid(IsaacMetadataAuxiliaryBinding.DEVELOPMENT));
 
         return editCoordinate;
     }
